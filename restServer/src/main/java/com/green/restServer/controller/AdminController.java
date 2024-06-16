97edf8b53ee5ccbd2ed127541d0077b1a1591f7a
KwonH97 <rnjsgus21@naver.com>
2024-06-16 23:36:56 +0900

    관리자 폼, 컨트롤러

---
+++ restServer/src/main/java/com/green/restServer/controller/AdminController.java
@@ -0,0 +1,30 @@
+package com.green.restServer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+	@PostMapping("/help")
+	public String regHelp() {
+		
+		return "";
+	}
+	
+	
+	@GetMapping("/help")
+	public String getHelpDetail() {
+		
+		return "";
+	}
+	
+	@GetMapping("/help")
+	public String getHelpList() {
+		
+		return "";
+	}
+	
+	
+}
