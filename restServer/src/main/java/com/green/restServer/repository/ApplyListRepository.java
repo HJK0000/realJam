3168a85d08615799ee4fe805e604ebc3ab631e9d
HJK0000 <queen00220@naver.com>
2024-06-19 18:18:42 +0900

    포지션 매칭 완성 & 지원받은 이력서 조회 완성

--- restServer/src/main/java/com/green/restServer/repository/ApplyListRepository.java
+++ restServer/src/main/java/com/green/restServer/repository/ApplyListRepository.java
@@ -3,6 +3,7 @@ package com.green.restServer.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -17,5 +18,7 @@ public interface ApplyListRepository extends JpaRepository<ApplyList, Long>{
 	
 	@Query(value="select * from apply_list where user_username= :user_username", nativeQuery=true)
 	public List<ApplyList> findByUserUsername(@Param("user_username")String username);
-	
+
+	@Query(value="UPDATE apply_list SET STATUS = 1 WHERE ano = :ano", nativeQuery=true)
+	public void updateApplyListStatusToAccepted(@Param("ano") Long ano); 
 }
